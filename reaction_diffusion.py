cae2dbf9a6f78d0e4a1583eb141e63efc29ea648
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2021-02-21 11:43:10 +0200

    Clean up implementation. Add commets.

--- reaction_diffusion.py
+++ reaction_diffusion.py
@@ -1,10 +1,9 @@
 import numpy as np
 import scipy.signal as sl
-import streamlit as st
 import matplotlib.pyplot as plt
 
-# init params
-iters = 5000
+# Init params
+iters = 6000
 d_A = 1.0
 d_B = 0.5
 f = 0.055
@@ -19,36 +18,32 @@ kernel = np.asarray([
     [.05, .2, .05],
     [ .2, -1, .2 ],
     [.05, .2, .05]
-])  # 3x3 convolution
+])
 
 np.random.seed(42)
+A = np.ones(size) + np.random.rand(*size) * 0.1
+B = np.zeros(size) + np.random.rand(*size) * 0.1
 
-A = np.ones(size)# + np.random.rand(*size) * 0.01
-B = np.zeros(size)# + np.random.rand(*size) * 0.01
-# B_ones_frac = 0.005  # fraction of B == 1
-# B_ones_n = int(h + w * B_ones_frac)
-# mask = (np.random.randint(0, h, B_ones_n), np.random.randint(0, w, B_ones_n))
-# B[mask] = 1.0
+# Add randomness to the pattern
+B_ones_frac = 0.005  # fraction of B == 1
+B_ones_n = int(h + w * B_ones_frac)
+mask = (np.random.randint(0, h, B_ones_n), np.random.randint(0, w, B_ones_n))
+B[mask] = 1.0
+
+# Add a square in the center of the image
+fraction = 0.1
 h2 = h // 2
 w2 = w // 2
-_h = max(1, int(h * 0.2))
-_w = max(1, int(w * 0.2))
-
-print(h2, _h, w2, _w)
-
+_h = max(1, int(h * fraction))
+_w = max(1, int(w * fraction))
 B[h2-_h:h2+_h, w2-_w:w2+_w] = 1
-# B[h2-_h + 1:h2+_h - 1, w2-_w + 1:w2+_w - 1] = 0  # fill the inner area with o
+B[h2-_h + 1:h2+_h - 1, w2-_w + 1:w2+_w - 1] = 0  # fill the inner area with o
 
-print(B)
-
-# update initial matrix in a loop
+# Solve the equation in a loop
 plt.figure()
 for i in range(iters):
-    # Laplace ...
-    diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='wrap')
+    diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='wrap')  # 2D Laplacian convolution
     diffusion_B = d_B * sl.convolve2d(B, kernel, mode='same', boundary='wrap')
-    # diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='fill', fillvalue=1)
-    # diffusion_B = d_B * sl.convolve2d(B, kernel, mode='same', boundary='fill')
 
     reaction_A = A * B**2
     reaction_B = A * B**2
@@ -59,18 +54,18 @@ for i in range(iters):
     A += (diffusion_A - reaction_A + feed_A) * dt
     B += (diffusion_B + reaction_B - kill_B) * dt
 
-    # A = np.clip(A, 0, 1)
-    # B = np.clip(B, 0, 1)
+    # Visualize interm results
+    n = iters // 10
+    if not i % n:
+        plt.subplot(2, 5, i // n + 1)
+        plt.axis('off')
+        plt.imshow(B, cmap=plt.cm.PRGn)
 
-    # if not i % 10:
-    #     plt.subplot(5, 2, i // 10 + 1)
-    #     plt.axis('off')
-    #     plt.imshow(B, cmap=plt.cm.PRGn, extent=[-1,1,-1,1])
-
-# plt.show()
+plt.tight_layout()
+plt.show()
 
 
-# visualize the heatmap
+# Visualize the final result heatmap
 plt.axis('off')
-plt.imshow(B, cmap=plt.cm.PRGn, extent=[-1,1,-1,1])
+plt.imshow(B, cmap=plt.cm.PRGn)
 plt.show()
