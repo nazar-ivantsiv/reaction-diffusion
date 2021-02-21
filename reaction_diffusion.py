54b1f142aefc4e6058e273ac611d4acb0f471031
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2021-02-21 11:19:23 +0200

    Working GS algorithm

--- reaction_diffusion.py
+++ reaction_diffusion.py
@@ -4,11 +4,11 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # init params
-iters =99
+iters = 5000
 d_A = 1.0
 d_B = 0.5
-f = 0.55
-k = 0.62
+f = 0.055
+k = 0.062
 dt = 1.0
 
 h = 200
@@ -31,8 +31,8 @@ B = np.zeros(size)# + np.random.rand(*size) * 0.01
 # B[mask] = 1.0
 h2 = h // 2
 w2 = w // 2
-_h = max(1, int(h * 0.1))
-_w = max(1, int(w * 0.1))
+_h = max(1, int(h * 0.2))
+_w = max(1, int(w * 0.2))
 
 print(h2, _h, w2, _w)
 
@@ -45,7 +45,7 @@ print(B)
 plt.figure()
 for i in range(iters):
     # Laplace ...
-    diffusion_A = d_A * sl.convolve2d(A, sundialkernel, mode='same', boundary='wrap')
+    diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='wrap')
     diffusion_B = d_B * sl.convolve2d(B, kernel, mode='same', boundary='wrap')
     # diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='fill', fillvalue=1)
     # diffusion_B = d_B * sl.convolve2d(B, kernel, mode='same', boundary='fill')
@@ -59,41 +59,18 @@ for i in range(iters):
     A += (diffusion_A - reaction_A + feed_A) * dt
     B += (diffusion_B + reaction_B - kill_B) * dt
 
-    A = np.clip(A, 0, 1)
-    B = np.clip(B, 0, 1)
+    # A = np.clip(A, 0, 1)
+    # B = np.clip(B, 0, 1)
 
-    # u,v = A[1:-1,1:-1], B[1:-1,1:-1]
-    # # u = A
-    # # v = B
-    # Du = d_A
-    # Dv = d_B
-    # F = f
-    # k = k
+    # if not i % 10:
+    #     plt.subplot(5, 2, i // 10 + 1)
+    #     plt.axis('off')
+    #     plt.imshow(B, cmap=plt.cm.PRGn, extent=[-1,1,-1,1])
 
-    # def laplacian_operator(U,V,dx):
-    #     Lu = (U[0:-2,1:-1] + U[1:-1,0:-2] + U[1:-1,2:] + U[2:,1:-1] - 4*U[1:-1,1:-1])/dx**2
-    #     Lv = (V[0:-2,1:-1] + V[1:-1,0:-2] + V[1:-1,2:] + V[2:,1:-1] - 4*V[1:-1,1:-1])/dx**2
-    #     return Lu,Lv
-
-    # Lu,Lv = laplacian_operator(A,B,1)
-    # uvv = u*v*v
-    # su = Du*Lu - uvv + F *(1-u)
-    # sv = Dv*Lv + uvv - (F+k)*v
-    # u += dt*su
-    # v += dt*sv
-
-    # A = u
-    # B = v
-
-    if not i % 10:
-        plt.subplot(5, 2, i // 10 + 1)
-        plt.axis('off')
-        plt.imshow(B)
-
-plt.show()
+# plt.show()
 
 
 # visualize the heatmap
-plt.imshow(B)
-plt.tight_layout()
+plt.axis('off')
+plt.imshow(B, cmap=plt.cm.PRGn, extent=[-1,1,-1,1])
 plt.show()
