bea2c485b18eb988f602a108a414f601b4919841
nazar-ivantsiv <nazar.ivantsiv@gmail.com>
2021-02-27 00:00:19 +0200

    Optimize torch implementation. Update controls. Clean up.

--- reaction_diffusion_streamlit.py
+++ reaction_diffusion_streamlit.py
@@ -1,7 +1,6 @@
 import time
 import numpy as np
 import pandas as pd
-# import scipy.signal as sl
 import torch
 import torch.nn.functional as F
 
@@ -17,34 +16,42 @@ def main():
         session.run_id += 1
 
     # Init params
-    random_noise = st.sidebar.checkbox('random_noise', value=False, key=session.run_id)
-    draw_square = st.sidebar.checkbox('draw_square', value=True, key=session.run_id)
+    random_noise = st.sidebar.checkbox('init_with_random_noise', value=False, key=session.run_id)
+    if random_noise:
+        rng_noise_amount_plh = st.sidebar.empty()
+        rng_seed_plh = st.sidebar.empty()
+    draw_square = st.sidebar.checkbox('init_with_square', value=True, key=session.run_id)
+    if draw_square:
+        square_ratio_plh = st.sidebar.empty()
+    if (not random_noise) and (not draw_square):
+        st.warning('Please initialize with random noise or square')
     iters = st.sidebar.slider('iterations', min_value=10, max_value=10000, step=10, value=6000, key=session.run_id)
-    d_A = st.sidebar.slider('d_A', min_value=0.0, max_value=2.0, step=0.1, value=1.0, key=session.run_id)
-    d_B = st.sidebar.slider('d_B', min_value=0.0, max_value=2.0, step=0.1, value=0.5, key=session.run_id)
-    f = st.sidebar.slider('feed_rate', min_value=0.0, max_value=0.5, step=0.005, value=0.055, key=session.run_id)
-    k = st.sidebar.slider('kill_rate', min_value=0.0, max_value=0.5, step=0.005, value=0.062, key=session.run_id)
-    # dt = st.sidebar.slider('dt', min_value=0.1, max_value=1.0, step=0.1, value=1.0, key=session.run_id)
+    f = st.sidebar.slider('feed_rate', min_value=0.03, max_value=0.1, step=0.0001, value=0.055, key=session.run_id)  # 0.055
+    k = st.sidebar.slider('kill_rate', min_value=0.03, max_value=0.1, step=0.0001, value=0.062, key=session.run_id)  # 0.062
+    d_A = st.sidebar.slider('d_A', min_value=0.5, max_value=1.3, step=0.1, value=1.0, key=session.run_id)
+    d_B = st.sidebar.slider('d_B', min_value=0.3, max_value=1.0, step=0.1, value=0.5, key=session.run_id)
+    img_size = st.sidebar.slider('img_size', min_value=10, max_value=1000, step=1, value=200, key=session.run_id)
+    cmap = st.sidebar.selectbox('plot_colormap', ['binary', 'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'], index=0,  key=session.run_id)
     dt = 1.0
-    side = st.sidebar.slider('side', min_value=10, max_value=1000, step=1, value=200, key=session.run_id)
 
-    h = side
-    w = side
+    h = img_size
+    w = img_size
     size = (h, w)
-
-    kernel = np.asarray([
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    kernel = torch.tensor([
         [.05, .2, .05],
         [ .2, -1, .2 ],
         [.05, .2, .05]
-    ])
+    ], device=device)
 
-    A = np.ones(size)
-    B = np.zeros(size)
+    A = torch.ones(size, device=device)
+    B = torch.zeros(size, device=device)
 
     # Add randomness to the pattern
     if random_noise:
-        rng_seed = st.sidebar.slider('rng_seed', min_value=0, max_value=1000, step=10, value=40, key=session.run_id)
-        rng_noise_amount = st.sidebar.slider('rng_noise_amount', min_value=0.0, max_value=0.5, step=0.005, value=0.005, key=session.run_id)
+        rng_seed = rng_seed_plh.slider('rng_seed', min_value=0, max_value=1000, step=10, value=40, key=session.run_id)
+        rng_noise_amount = rng_noise_amount_plh.slider('rng_noise_amount', min_value=0.0, max_value=0.5, step=0.005, value=0.005, key=session.run_id)
         np.random.seed(rng_seed)
 
         B_ones_frac = rng_noise_amount  # fraction of B == 1
@@ -54,81 +61,44 @@ def main():
 
     # Add a square in the center of the image
     if draw_square:
-        fraction = 0.1
+        square_ratio = square_ratio_plh.slider('square_ratio', min_value=0.05, max_value=1.0, step=0.05, value=0.1, key=session.run_id)
         h2 = h // 2
         w2 = w // 2
-        _h = max(1, int(h * fraction))
-        _w = max(1, int(w * fraction))
+        _h = max(1, int(h * square_ratio))
+        _w = max(1, int(w * square_ratio))
         B[h2-_h:h2+_h, w2-_w:w2+_w] = 1
-        B[h2-_h + 1:h2+_h - 1, w2-_w + 1:w2+_w - 1] = 0  # fill the inner area with o
+        B[h2-_h + 1:h2+_h - 1, w2-_w + 1:w2+_w - 1] = 0  # fill the inner area with 0
 
-    st.sidebar.write(f'Current parameters:')
-    st.sidebar.dataframe(pd.DataFrame(dict(iters=iters, d_A=d_A, d_B=d_B), index=[0]))
-    st.sidebar.dataframe(pd.DataFrame(dict(feed=f, kill=k, dt=dt), index=[0]))
-
-    # Solve the equation in a loop
     t0 = time.perf_counter()
-    fig1 = plt.figure()
+    fig1 = plt.figure(figsize=(4,4))
+    gs = fig1.add_gridspec(2, 5, wspace=0, hspace=0, top=0.5)
 
-    # A, B, interim_b = solve(A, B, kernel, d_A, d_B, f, k, dt, iters)
-    A, B, interim_b = solve_torch(A, B, kernel, d_A, d_B, f, k, dt, iters)
+    # Simulate pattern
+    with st.spinner('Processing...'):
+        A, B, interim_b = simulate_torch(A, B, kernel, d_A, d_B, f, k, dt, iters)
 
     # Visualize interim results
     for i, b in enumerate(interim_b):
-        plt.subplot(2, 5, i+1)
+        plt.subplot(gs[i])
         plt.axis('off')
-        plt.imshow(b, cmap=plt.cm.PRGn)
+        plt.imshow(b, cmap=cmap)
 
 
     # Visualize the final result heatmap
     fig2 = plt.figure()
     plt.axis('off')
-    plt.imshow(B, cmap=plt.cm.PRGn)
-    plt.colorbar()
-    st.pyplot(fig2)
+    plt.imshow(B, cmap=cmap)
+    # plt.colorbar()
+    st.pyplot(fig2, clear_figure=True)
 
     # Visualize interm results
-    # plt.tight_layout()
-    st.pyplot(fig1)
+    st.pyplot(fig1, clear_figure=True)
 
     st.write(f'Elapsed: {time.perf_counter() - t0:.2f} sec')
+    st.write(f'Current parameters:', pd.DataFrame(dict(iters=iters, d_A=d_A, d_B=d_B, feed=f, kill=k, dt=dt), index=[0]))
 
 
-# def solve(A, B, kernel, d_A, d_B, f, k, dt, iters):
-#     interim_b = []
-#     for i in range(iters):
-#         diffusion_A = d_A * sl.convolve2d(A, kernel, mode='same', boundary='wrap')  # 2D Laplacian convolution
-#         diffusion_B = d_B * sl.convolve2d(B, kernel, mode='same', boundary='wrap')
-
-#         reaction_A = A * B**2
-#         reaction_B = A * B**2
-
-#         feed_A = f * (1 - A)
-#         kill_B = (k + f) * B
-
-#         A += (diffusion_A - reaction_A + feed_A) * dt
-#         B += (diffusion_B + reaction_B - kill_B) * dt
-
-#         # Save interim results
-#         n = iters // 10
-#         if not i % n:
-#             interim_b.append(B)
-
-#     return A, B, interim_b
-
-
-def solve_torch(A, B, kernel, d_A, d_B, f, k, dt, iters):
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#    print(device)
-
-    A_t = torch.tensor(A, device=device)
-    B_t = torch.tensor(B, device=device)
-    kernel_t = torch.tensor(kernel, device=device)
-
-#    A_t = torch.from_numpy(A).float().to(device)
-#    B_t = torch.from_numpy(B).float().to(device)
-#    kernel_t = torch.from_numpy(kernel).float().to(device)
+def simulate_torch(A_t, B_t, kernel_t, d_A, d_B, f, k, dt, iters):
 
     A_t = A_t.expand(1, 1, -1, -1)
     B_t = B_t.expand(1, 1, -1, -1)
